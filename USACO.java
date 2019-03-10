925df28ffadd07384ee4906d1da663894b986d6a
sxiao1 <sxiao1@stuy.edu>
2019-03-09 22:54:48 -0500

    made progress on silver

--- USACO.java
+++ USACO.java
@@ -4,19 +4,19 @@ public class USACO{
   public static int[][] pasture;
 
   public static int bronze(String filename){
-    int instructions[] = new int[4];
+    int instructions[] = new int[4]; //4 values inputted
     ArrayList<Integer> nums = new ArrayList<Integer>();
     try{
       File text = new File(filename);
       Scanner in = new Scanner(text);
       while(in.hasNextInt()){
-        nums.add(in.nextInt());
+        nums.add(in.nextInt()); //adding all the values into an arraylist
       }
     }
     catch(FileNotFoundException e){}
     for(int i = 0; i < 4; i++){
-      instructions[i] = nums.get(0);
-      nums.remove(0);
+      instructions[i] = nums.get(0); //adding values to the array
+      nums.remove(0); //removing the first value to not mess up the index
     }
     int row = instructions[0];
     int col = instructions[1];
@@ -25,14 +25,14 @@ public class USACO{
     pasture = new int[row][col];
     for(int r = 0; r < row; r ++){
       for(int c = 0; c < col; c++){
-        pasture[r][c] = nums.get(0);
+        pasture[r][c] = nums.get(0); //copying over the numbers in the pasture
         nums.remove(0);
       }
     }
-    int[][] moves = new int[instructions[3]][3];
+    int[][] moves = new int[instructions[3]][3]; //array of all the moves
     for(int r = 0; r < moves.length; r++){
       for(int c = 0; c < moves[0].length; c++){
-        moves[r][c] = nums.get(0);
+        moves[r][c] = nums.get(0); //adding the values into the array
         nums.remove(0);
       }
     }
@@ -42,11 +42,11 @@ public class USACO{
     int finald = instructions[2];
     for(int r = 0; r < pasture.length; r++){
       for(int c = 0; c < pasture[0].length; c++){
-        if(pasture[r][c] > finald){
+        if(pasture[r][c] > finald){ //if greater than the final then it will be 0
           pasture[r][c] = 0;
         }
         else{
-          pasture[r][c] = finald - pasture[r][c];
+          pasture[r][c] = finald - pasture[r][c]; //if not replace value with difference
         }
       }
     }
@@ -60,11 +60,11 @@ public class USACO{
   }
   public static void stomping(int row, int col, int depth){
     int max = 0;
-    int newRow = row -1;
+    int newRow = row -1; //starts with row 1 and col 1 instead of 0
     int newCol = col -1;
     for(int r = 0; r < 3; r++){
       for(int c = 0; c < 3; c++){
-        if(pasture[newRow + r][newCol + c] > max){
+        if(pasture[newRow + r][newCol + c] > max){ //finding the largest value in a 3 x 3 square
           max = pasture[newRow + r][newCol + c];
         }
       }
@@ -72,7 +72,7 @@ public class USACO{
     int newd = max - depth;
     for(int r = 0; r < 3; r++){
       for(int c = 0; c < 3; c++){
-        if(pasture[newRow + r][newCol + c] > newd){
+        if(pasture[newRow + r][newCol + c] > newd){ //if larger than the depth, then its new value is the depth
           pasture[newRow + r][newCol + c] = newd;
         }
       }
@@ -87,5 +87,34 @@ public class USACO{
       newstr += "\n";
     }
     return newstr;
+  }
+  public static char[][] board;
+  public static int silver(String filename){
+    String together = "";
+    int[][] moves = {{0,1}, {0,-1}, {1,0}, {-1, 0}};
+    try{
+      File text = new File(filename);
+      Scanner in = new Scanner(text);
+      int rows = in.nextInt();
+      int cols = in.nextInt();
+      int time = in.nextInt();
+      in.nextLine();
+      String path = "";
+      while(in.hasNextLine()){
+        path = in.nextLine();
+        together += path;
+      }
+      int index = 0;
+      board = new char[rows][cols];
+      for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+          board[r][c] = together.charAt(index);
+          index++;
+        }
+      }
+      String[] nums = path.split(" ");
+    }
+    catch(FileNotFoundException e){}
+    return 1;
   }
 }
