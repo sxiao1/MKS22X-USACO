39c2e2dd8356468e529d07b90c2058e28b61175c
sxiao1 <sxiao1@stuy.edu>
2019-03-09 21:23:24 -0500

    finished stomping method and finishing up bronze

--- USACO.java
+++ USACO.java
@@ -30,7 +30,36 @@ public class USACO{
       }
     }
     int[][] moves = new int[instructions[3]][3];
+    for(int r = 0; r < moves.length; r++){
+      for(int c = 0; c < moves[0].length; c++){
+        moves[r][c] = nums.get(0);
+        nums.remove(0);
+      }
+    }
+    for(int i = 0; i < moves.length; i++){
+      stomping(moves[i][0],moves[i][1],moves[i][2]);
+    }
     return 1;
+  }
+  public static void stomping(int row, int col, int depth){
+    int max = 0;
+    int newRow = row -1;
+    int newCol = col -1;
+    for(int r = 0; r < 3; r++){
+      for(int c = 0; c < 3; c++){
+        if(pasture[newRow + r][newCol + c] > max){
+          max = pasture[newRow + r][newCol + c];
+        }
+      }
+    }
+    int newd = max - depth;
+    for(int r = 0; r < 3; r++){
+      for(int c = 0; c < 3; c++){
+        if(pasture[newRow + r][newCol + c] > newd){
+          pasture[newRow + r][newCol + c] = newd;
+        }
+      }
+    }
   }
   public static String returnString(){
     String newstr = "";
